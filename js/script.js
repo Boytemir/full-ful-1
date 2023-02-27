db1a6b8e177cba7600219b4f2f65a7ac65efa678
Boytemir <boytemirxayitmurodov@gmail.com>
2023-02-27 12:14:17 +0500

    barchasni qilib quydim

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 let form__box = document.querySelector('.form-box');
 let btn = document.querySelector('.btn');
 let fa__x = document.querySelector('.fa-x');
-
+let button__card__btn = document.querySelector('.button-card__btn')
 btn.addEventListener('click', () => {
+    form__box.classList.add('active')
+})
+button__card__btn.addEventListener('click', () => {
     form__box.classList.add('active')
 })
 fa__x.addEventListener('click', () => {
